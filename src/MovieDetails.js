f217aebe95e74138433099e79cb08ad58747dad9
Joseph Richardson <richardsonjp93@gmail.com>
2021-05-04 13:07:53 +0100

    styling for movie details

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Button} from '@material-ui/core'
+import './MovieDetails.css'
 
 const MovieDetails = ({movie, setSelectedMovie}) => {
 
@@ -17,20 +18,26 @@ const MovieDetails = ({movie, setSelectedMovie}) => {
 
   return (
     <div className="movie-details-container">
-      <h1>{movieInfo.Title}</h1>
-      <img src={movieInfo.Poster} alt={movieInfo.Title}/>
-      <h3>Details</h3>
-      <ul>
-        <li>Released: {movieInfo.Released}</li>
-        <li>Genre: {movieInfo.Genre}</li>
-        <li>Rated: {movieInfo.Rated}</li>
-        <li>Runtime: {movieInfo.Runtime}</li>
-        <li>Director: {movieInfo.Director}</li>
-        <li>IMDB Rating: {movieInfo.imdbRating}</li>
-      </ul>
-      <h3>Summary</h3>
-      <p>{movieInfo.Plot}</p>
-      <Button variant="contained" color="Primary" onClick={()=> {setSelectedMovie('')}} >
+      <h1 className="movie-details-title">{movieInfo.Title}</h1>
+      <div className="movie-details-poster-container">
+        <img className="movie-details-poster" src={movieInfo.Poster} alt={movieInfo.Title}/>
+      </div>
+      <div className="movie-details-details">
+        <h3>Details</h3>
+        <ul className="movie-details-list">
+          <li>Released: {movieInfo.Released}</li>
+          <li>Genre: {movieInfo.Genre}</li>
+          <li>Rated: {movieInfo.Rated}</li>
+          <li>Runtime: {movieInfo.Runtime}</li>
+          <li>Director: {movieInfo.Director}</li>
+          <li>IMDB Rating: {movieInfo.imdbRating}</li>
+        </ul>
+      </div>  
+      <div className="movie-details-summary">
+        <h3>Summary</h3>
+        <p>{movieInfo.Plot}</p>
+      </div>          
+      <Button className="movie-details-button" variant="contained" color="Primary" onClick={()=> {setSelectedMovie('')}} >
         Back
       </Button>
     </div>
